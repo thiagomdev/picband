cf5f4139752499fce9c9b47a0933b22f5165bae0
Thiago <messa.contato@gmail.com>
2021-08-08 18:29:00 -0300

    fix extension and alert

--- PicBand/Features/Register/Controller/RegisterViewController.swift
+++ PicBand/Features/Register/Controller/RegisterViewController.swift
@@ -33,38 +33,6 @@ class RegisterViewController: UIViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = false
     }
 }
-//
-//extension RegisterViewController: RegisterScreenDelegate {
-//
-//    func registerButton() {
-//        self.auth?.createUser(withEmail: self.registerScreen?.textFieldEmail.text ?? "", password: self.registerScreen?.textFieldPassword.text ?? "", completion: { result, error in
-//
-//            let alert = UIAlertController(title: "Alert!", message: "Usuário Existente.", preferredStyle: .alert)
-//
-//            let action = UIAlertAction(title: "Fechar", style: .default) {
-//                (action) in
-//            }
-//
-//            let alert2 = UIAlertController(title: "Success", message: "Cadastro realizado com Sucesso.", preferredStyle: .alert)
-//
-//            let actionSucess = UIAlertAction(title: "Fechar!", style: .default) {
-//                (action) in
-//            }
-//
-//            alert.addAction(action)
-//            alert2.addAction(actionSucess)
-//
-//            if error != nil {
-//                print("Usuário existente.")
-//                self.present(alert, animated: true, completion: nil)
-//
-//            } else {
-//                print("Cadastro realizado com Sucesso.")
-//                self.present(alert2, animated: true, completion: nil)
-//                self.dismiss(animated: true, completion: nil)
-//            }
-//        })
-//    }
-//}
+
 
 
