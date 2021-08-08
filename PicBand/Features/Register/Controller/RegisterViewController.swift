530f7d39844cb19f653b01db0e0023bb0f4ea068
Thiago <messa.contato@gmail.com>
2021-08-08 17:52:25 -0300

    Alert fix and added extension folder to registerViewController

--- PicBand/Features/Register/Controller/RegisterViewController.swift
+++ PicBand/Features/Register/Controller/RegisterViewController.swift
@@ -12,8 +12,8 @@ class RegisterViewController: UIViewController {
     
     // MARK: - Properties
     var registerScreen: RegisterScreen?
-    
     var auth: Auth?
+    
     // MARK: - Super Methods
     override func loadView() {
         self.registerScreen = RegisterScreen()
@@ -33,38 +33,38 @@ class RegisterViewController: UIViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = false
     }
 }
-
-extension RegisterViewController: RegisterScreenDelegate {
-    
-    func registerButton() {
-        self.auth?.createUser(withEmail: self.registerScreen?.textFieldEmail.text ?? "", password: self.registerScreen?.textFieldPassword.text ?? "", completion: { result, error in
-            
-            let alert = UIAlertController(title: "Alert!", message: "Usuário Existente.", preferredStyle: .alert)
-            
-            let action = UIAlertAction(title: "Fechar", style: .default) {
-                (action) in
-            }
-            
-            let alert2 = UIAlertController(title: "Success", message: "Cadastro realizado com Sucesso.", preferredStyle: .alert)
-            
-            let actionSucess = UIAlertAction(title: "Fechar!", style: .default) {
-                (action) in
-            }
-            
-            alert.addAction(action)
-            alert2.addAction(actionSucess)
-            
-            if error != nil {
-                print("Usuário existente.")
-                self.present(alert, animated: true, completion: nil)
-                
-            } else {
-                print("Cadastro realizado com Sucesso.")
-                self.present(alert2, animated: true, completion: nil)
-                self.dismiss(animated: true, completion: nil)
-            }
-        })
-    }
-}
+//
+//extension RegisterViewController: RegisterScreenDelegate {
+//
+//    func registerButton() {
+//        self.auth?.createUser(withEmail: self.registerScreen?.textFieldEmail.text ?? "", password: self.registerScreen?.textFieldPassword.text ?? "", completion: { result, error in
+//
+//            let alert = UIAlertController(title: "Alert!", message: "Usuário Existente.", preferredStyle: .alert)
+//
+//            let action = UIAlertAction(title: "Fechar", style: .default) {
+//                (action) in
+//            }
+//
+//            let alert2 = UIAlertController(title: "Success", message: "Cadastro realizado com Sucesso.", preferredStyle: .alert)
+//
+//            let actionSucess = UIAlertAction(title: "Fechar!", style: .default) {
+//                (action) in
+//            }
+//
+//            alert.addAction(action)
+//            alert2.addAction(actionSucess)
+//
+//            if error != nil {
+//                print("Usuário existente.")
+//                self.present(alert, animated: true, completion: nil)
+//
+//            } else {
+//                print("Cadastro realizado com Sucesso.")
+//                self.present(alert2, animated: true, completion: nil)
+//                self.dismiss(animated: true, completion: nil)
+//            }
+//        })
+//    }
+//}
 
 
---
+++ PicBand/Features/Register/Extensions/ExtensionRegisterViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ExtensionRegisterViewController.swift
+//  ExtensionRegisterViewController
+//
+//  Created by Thiago Monteiro on 08/08/21.
+//
+
+import UIKit
+import Firebase
+
+extension RegisterViewController: RegisterScreenDelegate {
+    
+    func registerButton() {
+        self.auth?.createUser(withEmail: self.registerScreen?.textFieldEmail.text ?? "", password: self.registerScreen?.textFieldPassword.text ?? "", completion: { result, error in
+            
+            let alert = UIAlertController(title: "Alert!", message: "Usuário Existente.", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Fechar", style: .default) {
+                (action) in
+            }
+            
+            let alert2 = UIAlertController(title: "Success", message: "Cadastro realizado com Sucesso.", preferredStyle: .alert)
+            
+            let actionSucess = UIAlertAction(title: "Fechar!", style: .default) {
+                (action) in
+            }
+            
+            alert.addAction(action)
+            alert2.addAction(actionSucess)
+            
+            if error != nil {
+                print("Usuário existente.")
+                self.present(alert, animated: true, completion: nil)
+                
+            } else {
+                print("Cadastro realizado com Sucesso.")
+                self.present(alert2, animated: true, completion: nil)
+                self.registerScreen?.textFieldEmail.text = ""
+                self.registerScreen?.textFieldPassword.text = ""
+                self.dismiss(animated: true, completion: nil)
+            }
+        })
+    }
+}
